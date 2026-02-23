c256090c39e9e509433a67a8f04604ea1f32152c
Loom Agent <loom@localhost>
2026-02-23 05:56:50 +0000

    [WIP] Auto-checkpoint after file changes
    Bead: ac-130
    Agent: exec-aviation-211134d5

--- packages/g1000-rendering/src/index.ts
+++ packages/g1000-rendering/src/index.ts
@@ -1,3 +1,11 @@
+// Entry point for G1000 Rendering SDK
+
+export * from './primitives';
+export * from './pfd';
+export * from './mfd';
+export * from './themes';
+export * from './utils';
+
 export const render = () => {
   console.log('Rendering G1000 display');
 };
